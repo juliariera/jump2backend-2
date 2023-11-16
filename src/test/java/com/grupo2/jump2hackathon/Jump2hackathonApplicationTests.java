96f67b882975586f9be3039eb8c8ff8dca7f60b3
Luiso-o <cheportillo@gmail.com>
2023-11-16 13:37:10 +0100

    implementación Swagger

--- src/test/java/com/grupo2/jump2hackathon/Jump2hackathonApplicationTests.java
+++ src/test/java/com/grupo2/jump2hackathon/Jump2hackathonApplicationTests.java
@@ -1,13 +1,8 @@
 package com.grupo2.jump2hackathon;
 
-import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Jump2hackathonApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
 }
