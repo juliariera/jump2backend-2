469dbf42564f0548a88e92e0a78cbe6523992624
PauSansa <pausansa59@gmail.com>
2023-11-16 10:56:23 +0100

    first commit

---
+++ src/main/java/com/grupo2/jump2hackathon/Jump2hackathonApplication.java
@@ -0,0 +1,13 @@
+package com.grupo2.jump2hackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jump2hackathonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jump2hackathonApplication.class, args);
+	}
+
+}
