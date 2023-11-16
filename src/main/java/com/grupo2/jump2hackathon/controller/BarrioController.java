6e1001025591ed71d0ad802581ac06043bf630b0
Luiso-o <cheportillo@gmail.com>
2023-11-16 13:02:42 +0100

    first commit

--- src/main/java/com/grupo2/jump2hackathon/controller/BarrioController.java
+++ src/main/java/com/grupo2/jump2hackathon/controller/BarrioController.java
@@ -1,6 +1,5 @@
-package com.grupo2.jump2hackathon;
+package com.grupo2.jump2hackathon.controller;
 
-import lombok.Getter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
--- src/main/java/com/grupo2/jump2hackathon/repository/BarrioRepository.java
+++ src/main/java/com/grupo2/jump2hackathon/repository/BarrioRepository.java
@@ -2,9 +2,10 @@ package com.grupo2.jump2hackathon.repository;
 
 import com.grupo2.jump2hackathon.documents.Barrio;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
-//Develop
+@Repository
 public interface BarrioRepository extends MongoRepository<Barrio, UUID> {
 }
